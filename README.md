c8bf16ff9cf8015ff1e4e4be2fbbd24668638c1d
ivteplo <iteplov7@gmail.com>
2022-03-08 11:30:06 +0200

    Start working on layout

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # Pomodo
-
-Pomodoro app to make you more productive
-
-## Features
-
-- Timer duration customization
-- Light and Dark modes
-- Notifications
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+// Copyright (c) 2021-2022 Ivan Teplov
+
+<template>
+  <div class="App fill column">
+    <header class="column">
+      <h1>Pomodo</h1>
+    </header>
+
+    <p>TODO</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+
+  },
+}
+</script>
+
+<style scoped>
+.App {
+  justify-content: center;
+  align-items: center;
+}
+</style>
